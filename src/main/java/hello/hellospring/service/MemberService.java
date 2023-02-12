b1018c4aed96303d463b84bd8d5a513043506dd4
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-02-13 03:16:52 +0900

    스프링 빈과 의존관계 -자바코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,17 +3,22 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
+
+    public MemberService(MemoryMemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
 
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원X
