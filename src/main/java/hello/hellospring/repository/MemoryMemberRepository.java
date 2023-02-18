14b8c6694d06bdd6dad38c92b28274a354b8c043
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-02-19 03:28:11 +0900

    스프링 DB 접근기술 - 순수 JDBC

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,12 +1,9 @@
 package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
-import org.springframework.stereotype.Repository;
-
 import java.util.*;
 /**
  * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
  */
-
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
@@ -20,18 +17,16 @@ public class MemoryMemberRepository implements MemberRepository {
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
-
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
-    @Override
-    public List<Member> findAll() {
-        return new ArrayList<>(store.values());
-    }
     public void clearStore() {
         store.clear();
-    }
-}
+    } }
